890a454339ec81714a163a63c82e4906f6e77550
Thaís Moreira <thaisdmedeiros@gmail.com>
2024-10-08 15:36:14 +0200

    Revert "update freelance-hours"
    This reverts commit d2c44217e0c4add4b68a71e66512e2168ce64404.

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use function Pest\Laravel\get;
-
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/outra-rota', fn() => 'oi');

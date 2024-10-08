d2c44217e0c4add4b68a71e66512e2168ce64404
Thaís Moreira <thaisdmedeiros@gmail.com>
2024-10-08 15:24:55 +0200

    update freelance-hours
    - add phpinfo exit killed route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+phpinfo();
+
+exit;
+
 use Illuminate\Support\Facades\Route;
+
+use function Pest\Laravel\get;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/outra-rota', fn() => 'oi');

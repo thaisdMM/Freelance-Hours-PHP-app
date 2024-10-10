c1c6ef66740bcb14c03648d9f6aa04213ed06dbc
Thaís Moreira <thaisdmedeiros@gmail.com>
2024-10-10 19:48:56 +0200

    Update WelcomeController.php

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,34 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class WelcomeController extends Controller
 {
     public function __invoke(Request $request)
-    
     {
-
-      $user = User::find(1);
-
-      /*
-      User::query()->create([
-
-         'name' => 'Thais',
-
-         'email' => 'thais@email.com',
-
-         'password' => 'jeremias',
-     
-     
-      ]); */
-
-      Auth::login($user);
-
-      return view('teste.thais');
-      
-
+        return view('welcome');
     }
 }

6512dd7a8b36144ecfa86bb8e633c9afe7771fd9
Thaís Moreira <thaisdmedeiros@gmail.com>
2024-10-10 19:52:50 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,8 +20,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'email_verified_at'
-    ];
+          ];
 
     /**
      * The attributes that should be hidden for serialization.

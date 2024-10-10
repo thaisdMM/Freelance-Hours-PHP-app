200cf5a5f59f82cadf5378d26eb1514770bf5f4a
Thaís Moreira <thaisdmedeiros@gmail.com>
2024-10-10 18:38:56 +0200

    knowing user model

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
     public function __invoke(Request $request)
+    
     {
+
+      $user = User::query()->create([
+
+         'name' => 'Thais',
+
+         'email' => 'thais@email.com',
+
+         'password' => 'jeremias',
+      ]);
+
+      dd($user);
 
       return view('teste.thais');
         

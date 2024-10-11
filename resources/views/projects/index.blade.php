fc0d8172774241ef58797b25dd3ef43b0e741c55
Thaís Moreira <thaisdmedeiros@gmail.com>
2024-10-11 19:01:10 +0200

    add index.blade and update web.php

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,10 @@
+<x-layouts.app>
+
+<a href="{{route('projects.show', 1)}}">
+
+Primeiro projeto
+
+</a>
+
+
+</x-layouts.app>
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,6 @@
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', WelcomeController::class);
+Route::view('/', 'projects.index')->name('projects.index');
+
+Route::view('/project/{project}', 'projects.show')->name('projects.show');

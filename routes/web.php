5100acf4d1ee06176c4678f7fc18cc74e9d26448
Thaís Moreira <thaisdmedeiros@gmail.com>
2024-10-12 10:53:32 +0200

    update routes/web.php and created ProjectsController

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProjectsController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'projects.index')->name('projects.index');
+Route::get('/', [ProjectsController::class, 'index'])->name('projects.index');
 
-Route::view('/project/{project}', 'projects.show')->name('projects.show');
+Route::get('/project/{project}', [ProjectsController::class, 'show'])->name('projects.show');

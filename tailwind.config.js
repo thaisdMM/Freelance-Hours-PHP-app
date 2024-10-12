75c86c98ac26e84e8d3600ab4bb1d20a38e760d4
Thaís Moreira <thaisdmedeiros@gmail.com>
2024-10-12 14:26:24 +0200

    created tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+   "./resources/**/*.blade.php",
+  ],
+  theme: {
+    extend: {
+
+      font: {
+         sans: ['Segoe UI', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+

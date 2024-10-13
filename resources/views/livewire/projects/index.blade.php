45f7a35a42a323c8d172d24fd78a8c714967c567
Thaís Moreira <thaisdmedeiros@gmail.com>
2024-10-13 17:31:53 +0200

    add projects/status.blade update project-card-simple

--- resources/views/livewire/projects/index.blade.php
+++ resources/views/livewire/projects/index.blade.php
@@ -1,13 +1,12 @@
 <div class="grid grid-cols-2 gap-4">
     
    @foreach ($this->projects as $project)
-
-   <li>
+   
           <a href="{{ route('projects.show', $project) }}" >
             
             <x-project-card-simple :$project />
          </a>
-   </li>
+   
    @endforeach
 
 </div>

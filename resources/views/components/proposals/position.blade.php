10e794e23e1e99f1417ca8a1f0662f5e93149c7e
Thaís Moreira <thaisdmedeiros@gmail.com>
2024-10-18 11:43:58 +0200

    Update position.blade.php

--- resources/views/components/proposals/position.blade.php
+++ resources/views/components/proposals/position.blade.php
@@ -6,18 +6,18 @@
     'bg-[#F1F5F9]' => $position > 2,
     ])>
     
-    @if ($position< 3)
+    @if ($position
+    < 3)
         <x-ui.icons.trophy class="w-[24px] h-[24px]" />
     @else
 
     @if ($status == 'up')
     <x-ui.icons.arrow-up class="w-[24px] h-[24px]" />
-
     @elseif($status == 'down')
     <x-ui.icons.arrow-down class="w-[24px] h-[24px]" />
-
     @else
     <x-ui.icons.minus class="w-[24px] h-[24px]" />
+    @endif
     @endif
 
 </div>
